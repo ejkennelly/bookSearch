6e3accea1e12cc15285b6ab8e23ab6daa348862a
EJ <elizakennelly@gmail.com>
2020-12-01 00:32:40 -0600

    updated form js

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,27 +1,28 @@
 import React from "react";
 
-// This file exports the Input, TextArea, and FormBtn components
-
-export function Input(props) {
-  return (
-    <div className="form-group">
-      <input className="form-control" {...props} />
+function Form(props){
+  return(
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="search">
+            <h2>Search for books and save those you're intrigued by!</h2>
+          </label>
+          <input onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search by book title"
+          id="search"
+          />
+          <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
+            Search
+          </button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
-export function TextArea(props) {
-  return (
-    <div className="form-group">
-      <textarea className="form-control" rows="20" {...props} />
-    </div>
-  );
-}
-
-export function FormBtn(props) {
-  return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
-      {props.children}
-    </button>
-  );
-}
+export default Form;
