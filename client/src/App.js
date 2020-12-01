37664f6e40a7f11a80c60d32eb3ff9e8a9631db5
EJ <elizakennelly@gmail.com>
2020-12-01 01:30:38 -0600

    updated app js and index js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path ="/search" component={Search} />
+            <Route exact path ="/saved" component={Saved} />
+            <Route exact path ="/noMatch" component={NoMatch} />
+
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
